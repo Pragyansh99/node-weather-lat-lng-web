af965bf88fbc59fa1b9f59f17dab20545bcd72d8
Pragyansh Dwivedi <pragyansh99@gmail.com>
2020-06-18 01:40:02 +0530

    Updated CSS

--- src/app.js
+++ src/app.js
@@ -19,59 +19,63 @@ const publicDirPath = path.join(__dirname, '../public')
 app.set('view engine', 'hbs')
 
 // in case changing views directory name and path
-const viewsPath = path.join(__dirname,'../template/views' )
-app.set('views',viewsPath)
+const viewsPath = path.join(__dirname, '../template/views')
+app.set('views', viewsPath)
 
 // Setup Partials
-const partialPath = path.join(__dirname,'../template/partials')
+const partialPath = path.join(__dirname, '../template/partials')
 hbs.registerPartials(partialPath)
 
 
 // Routes for dynamic templating
-app.get('', (req, res)=>{
-    res.render('index',{
-        'title':'Dynamic Templeting Using Handlebar View Engine',
-        'description':'Welcome to Homepage'
+app.get('', (req, res) => {
+    res.render('index', {
+        'title': 'Dynamic Templeting Using Handlebar View Engine',
+        'description': 'Welcome to Homepage'
     })
 })
 
-app.get('/about', (req, res)=>{
-    res.render('about',{
-        title:'About Us',
-        description:'Welcome to About us page'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us',
+        description: 'Welcome to About us page'
     })
 })
 
-app.get('/help', (req, res)=>{
-    res.render('about',{
-        title:'Helpdesk',
-        description:'Welcome to Help page'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Helpdesk',
+        description: 'Welcome to Help page'
     })
 })
 
 // Setting up query string 
 
-app.get('/forecast',(req,res)=>{
-    if(!req.query.search) {
+app.get('/forecast', (req, res) => {
+    if (!req.query.search) {
         return res.send({
-            error:'Please provide a location for forecasting weather'
+            error: 'Please provide a location for forecasting weather'
         })
     }
 
-    geocode(req.query.search, (error, {location, lattitude, longitude } = { } )=>{
-        if(error) {
+    geocode(req.query.search, (error, {
+        location,
+        lattitude,
+        longitude
+    } = {}) => {
+        if (error) {
             return res.send({
                 error_message: error
             })
         }
-        
+
         res.send({
             search: req.query.search,
             location,
             lattitude,
             longitude,
-            forecast:'Weather is cloudy here'
-        })  
+            forecast: 'Weather is cloudy here'
+        })
     })
 })
 
@@ -91,25 +95,25 @@ app.use(express.static(publicDirPath))
 //     res.send('Welcome to about page')
 // })
 
-app.get('/weather', (req, res)=>{
+app.get('/weather', (req, res) => {
     res.send('Welcome to weather page')
 })
 
-app.get('/about/*', (req, res)=>{
-   res.render('404', {
-       title:'Article Not found',
-       description:'Article you are looking for might be removed or moved to another page'
-   })
+app.get('/about/*', (req, res) => {
+    res.render('404', {
+        title: 'Article Not found',
+        description: 'Article you are looking for might be removed or moved to another page'
+    })
 })
 
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
     res.render('404', {
-        title:'Page Not found',
-        description:'The page you are looking for might be removed or moved to another location '
+        title: 'Page Not found',
+        description: 'The page you are looking for might be removed or moved to another location '
     })
 })
 
 // Initialize the server 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is up and running at port ${port}`)
 })
